d9fd69866645dd14faa2c7ac62d2b0c8bdc5da0b
Witek <witek.nowogorski@gmail.com>
2023-01-25 18:52:48 +0100

    .

--- gui.py
+++ gui.py
@@ -28,72 +28,16 @@ from PIL import Image
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
 import constelations
 import pandas
-
-
-class MapCanvas(FigureCanvas):
-    def __init__(self, parent):
-        fig, self.ax = plt.subplots(figsize=(5,4), dpi=200)
-        super().__init__(fig)
-        self.setParent(parent)
-
-        self.location = location.LocationApi.get_location()
-        with open(os.path.relpath("loc.json")) as loc_file:
-            observer_loc = json.load(loc_file)
-            olat = observer_loc["results"][1]['geometry']['location']['lat']
-            olon = observer_loc["results"][1]['geometry']['location']['lng']
-
-        utc_dt = datetime.datetime.now(tz=pytz.UTC)
-        self.time = load.timescale().utc(utc_dt)
-
-        self.observer = wgs84.latlon(latitude_degrees=olat, longitude_degrees=olon).at(self.time)
-        self.position = self.observer.from_altaz(alt_degrees=90, az_degrees=0)
-
-        ra, dec, distance = self.observer.radec()
-        center_object = Star(ra=ra, dec=dec)
-
-        eph = load('de421.bsp')
-        with load.open(hipparcos.URL) as f:
-            stars = hipparcos.load_dataframe(f)
-
-        earth = eph['earth']
-        center = earth.at(self.time).observe(center_object)
-        projection = build_stereographic_projection(center)
-        field_of_view_degrees = 180.0
-
-        star_positions = earth.at(self.time).observe(Star.from_dataframe(stars))
-        stars['x'], stars['y'] = projection(star_positions)
-        chart_size = 10
-
-        max_star_size = 50
-        limiting_magnitude = 10
-
-        bright_stars = (stars.magnitude <= limiting_magnitude)
-        magnitude = stars['magnitude'][bright_stars]
-
-        marker_size = max_star_size * 10 ** (magnitude / -2.5)
-
-        scatter = self.ax.scatter(stars['x'][bright_stars], stars['y'][bright_stars],
-                             s=marker_size, color='white', marker='.', linewidths=0,
-                             zorder=2)
-
-        border = plt.Circle((0, 0), 1, color='black', fill=True)
-        self.ax.add_patch(border)
-
-        horizon = Circle((0, 0), radius=1, transform=self.ax.transData)
-        for col in self.ax.collections:
-            col.set_clip_path(horizon)
-
-        self.ax.set_xlim(-1, 1)
-        self.ax.set_ylim(-1, 1)
-        plt.axis('off')
-        plt.show()
+from skymap import MapCanvas
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.resize(1600, 800)
 
-        chart = MapCanvas(self)
+        self.chart = MapCanvas(self)
+        self.chart.resize(700, 700)
+
 
 app = QtWidgets.QApplication(sys.argv)
 gui = MainWindow()
---
+++ skymap.py
@@ -0,0 +1,69 @@
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import json
+import datetime as datetime
+import pytz
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
+from matplotlib import *
+from skyfield.api import Star, load, wgs84, load_constellation_map, position_of_radec, load_constellation_names
+from skyfield.data import hipparcos, stellarium
+from skyfield.projections import build_stereographic_projection
+import location
+
+
+class MapCanvas(FigureCanvas):
+    def __init__(self, parent):
+        fig, self.ax = plt.subplots(figsize=(5,4), dpi=200)
+        super().__init__(fig)
+        self.setParent(parent)
+
+        self.location = location.LocationApi.get_location()
+        with open(os.path.relpath("loc.json")) as loc_file:
+            observer_loc = json.load(loc_file)
+            olat = observer_loc["results"][1]['geometry']['location']['lat']
+            olon = observer_loc["results"][1]['geometry']['location']['lng']
+
+        utc_dt = datetime.datetime.now(tz=pytz.UTC)
+        self.time = load.timescale().utc(utc_dt)
+
+        self.observer = wgs84.latlon(latitude_degrees=olat, longitude_degrees=olon).at(self.time)
+        self.position = self.observer.from_altaz(alt_degrees=90, az_degrees=0)
+
+        ra, dec, distance = self.observer.radec()
+        center_object = Star(ra=ra, dec=dec)
+
+        eph = load('de421.bsp')
+        with load.open(hipparcos.URL) as f:
+            stars = hipparcos.load_dataframe(f)
+
+        earth = eph['earth']
+        center = earth.at(self.time).observe(center_object)
+        projection = build_stereographic_projection(center)
+        field_of_view_degrees = 180.0
+
+        star_positions = earth.at(self.time).observe(Star.from_dataframe(stars))
+        stars['x'], stars['y'] = projection(star_positions)
+        chart_size = 10
+
+        max_star_size = 50
+        limiting_magnitude = 10
+
+        bright_stars = (stars.magnitude <= limiting_magnitude)
+        magnitude = stars['magnitude'][bright_stars]
+
+        marker_size = max_star_size * 10 ** (magnitude / -2.5)
+
+        scatter = self.ax.scatter(stars['x'][bright_stars], stars['y'][bright_stars],
+                             s=marker_size, color='white', marker='.', linewidths=0,
+                             zorder=2)
+
+        border = plt.Circle((0, 0), 1, color='black', fill=True)
+        self.ax.add_patch(border)
+
+        horizon = Circle((0, 0), radius=1, transform=self.ax.transData)
+        for col in self.ax.collections:
+            col.set_clip_path(horizon)
+
+        self.ax.set_xlim(-1, 1)
+        self.ax.set_ylim(-1, 1)
+        plt.axis('off')
