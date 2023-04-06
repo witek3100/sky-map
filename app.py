09bbed383972bcb1d5cc1d26f4402553f38f73b8
Witek <witek.nowogorski@gmail.com>
2023-04-06 23:01:36 +0200

    prototype

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+import base64
+from flask import Flask, render_template, request
+import map
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return render_template('main.html')
+    longtitude = request.args.get('longtitude')
+    latitude = request.args.get('latitude')
+    map_ = map.create_map(longtitude, latitude)
+    map_data = base64.b64encode(map_.getbuffer()).decode("ascii")
+    return render_template('main.html', map=map_data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ map.py
@@ -0,0 +1,139 @@
+import base64
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
+from skyfield.api import Star, load, wgs84, load_constellation_map, position_of_radec, load_constellation_names
+from skyfield.data import hipparcos, stellarium
+from skyfield.projections import build_stereographic_projection
+from io import BytesIO
+from matplotlib.figure import Figure
+import pytz
+import datetime
+utc_dt = datetime.datetime.now(tz=pytz.UTC)
+time = load.timescale().utc(utc_dt)
+
+def create_map(longtitude, latitude):
+    try:
+        lon = float(longtitude)
+        lat = float(latitude)
+    except ValueError:
+        lon = 0
+        lat = 0
+
+    fig = Figure(figsize=(10,10), dpi=100)
+    fig.set_facecolor((0,0,0))
+    ax = fig.subplots()
+    observer = wgs84.latlon(latitude_degrees=lat, longitude_degrees=lon).at(time)
+    position = observer.from_altaz(alt_degrees=90, az_degrees=0)
+
+    ra, dec, distance = observer.radec()
+    center_object = Star(ra=ra, dec=dec)
+
+    eph = load('de421.bsp')
+    with load.open(hipparcos.URL) as f:
+        stars = hipparcos.load_dataframe(f)
+
+    earth = eph['earth']
+    center = earth.at(time).observe(center_object)
+    projection = build_stereographic_projection(center)
+    field_of_view_degrees = 180.0
+
+    star_positions = earth.at(time).observe(Star.from_dataframe(stars))
+    stars['x'], stars['y'] = projection(star_positions)
+    chart_size = 10
+    max_star_size = 30
+    limiting_magnitude = 10
+    bright_stars = (stars.magnitude <= limiting_magnitude)
+    magnitude = stars['magnitude'][bright_stars]
+    marker_size = max_star_size * 10 ** (magnitude / -3.5)
+    scatter = ax.scatter(stars['x'][bright_stars], stars['y'][bright_stars],
+                                 s=marker_size, color='white', marker='.', linewidths=0,
+                                 zorder=2)
+
+    sun = eph['sun']
+    mercury = eph['MERCURY BARYCENTER']
+    venus = eph['VENUS BARYCENTER']
+    mars = eph['MARS BARYCENTER']
+    jupiter = eph['JUPITER BARYCENTER']
+    saturn = eph['SATURN BARYCENTER']
+    uranus = eph['URANUS BARYCENTER']
+    neptune = eph['NEPTUNE BARYCENTER']
+    moon = eph['moon']
+
+    mercury_position = earth.at(time).observe(mercury)
+    mercury_x, mercury_y = projection(mercury_position)
+
+    venus_position = earth.at(time).observe(venus)
+    venus_x, venus_y = projection(venus_position)
+
+    mars_position = earth.at(time).observe(mars)
+    mars_x, mars_y = projection(mars_position)
+
+    jupiter_position = earth.at(time).observe(jupiter)
+    jupiter_x, jupiter_y = projection(jupiter_position)
+
+    saturn_position = earth.at(time).observe(saturn)
+    saturn_x, saturn_y = projection(saturn_position)
+
+    uranus_position = earth.at(time).observe(uranus)
+    uranus_x, uranus_y = projection(uranus_position)
+
+    neptune_position = earth.at(time).observe(neptune)
+    neptune_x, neptune_y = projection(neptune_position)
+
+    moon_position = earth.at(time).observe(moon)
+    moon_x, moon_y = projection(moon_position)
+
+    sun_position = earth.at(time).observe(sun)
+    sun_x, sun_y = projection(sun_position)
+
+    ax.scatter(mercury_x, mercury_y,
+                            s=70, color='brown', marker='.', linewidths=0,
+                            zorder=2)
+
+    ax.scatter(venus_x, venus_y,
+                            s=90, color='orange', marker='.', linewidths=0,
+                            zorder=2)
+
+    ax.scatter(mars_x, mars_y,
+                       s=100, color='red', marker='.', linewidths=0,
+                       zorder=2)
+
+    ax.scatter(jupiter_x, jupiter_y,
+                            s=120, color='peru', marker='.', linewidths=0,
+                            zorder=2)
+
+    ax.scatter(saturn_x, saturn_y,
+                            s=110, color='grey', marker='.', linewidths=0,
+                            zorder=2)
+
+    ax.scatter(uranus_x, uranus_y,
+                       s=100, color='cyan', marker='.', linewidths=0,
+                       zorder=2)
+
+    ax.scatter(neptune_x, neptune_y,
+                            s=100, color='blue', marker='.', linewidths=0,
+                            zorder=2)
+
+    ax.scatter(moon_x, moon_y,
+                       s=150, color='grey', marker='o', linewidths=0,
+                       zorder=2)
+
+    sun = ax.scatter(sun_x, sun_y,
+                             s=500, color='yellow', marker='.', linewidths=0,
+                             zorder=2)
+
+    bckg = plt.Circle((0, 0), 4, color='black', fill=True)
+    ax.add_patch(bckg)
+    border = plt.Circle((0, 0), 0.99, color='grey', fill=False, linewidth=0.8)
+    ax.add_patch(border)
+    horizon = Circle((0, 0), radius=1, transform=ax.transData)
+    for col in ax.collections:
+        col.set_clip_path(horizon)
+
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.axis('off')
+    tmpfile = BytesIO()
+    fig.savefig(tmpfile, format='png')
+
+    return tmpfile
