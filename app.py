74e2971511c5c29287c17d970ff9d240996aacee
Witek <witek.nowogorski@gmail.com>
2023-04-06 17:27:18 +0200

    .

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
